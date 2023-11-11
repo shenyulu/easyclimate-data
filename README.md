f9148b65cd401459aa50111c9bf890747fe20c88
Shenyulu <59901836+shenyulu@users.noreply.github.com>
2023-11-11 20:16:41 +0800

    Add sample file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # easyclimate-data
- Data repository for easyclimate examples
+
+This repository exists to hold example datasets for [easylimate](https://github.com/shenyulu/easyclimate) (e.g., as used in the documentation) that would bloat the main repository if included there.
+
+> idea borrowed from [xarray](https://github.com/pydata/xarray)
---
+++ make_check_sums.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import glob
+
+def file_md5_checksum(fname):
+    import hashlib
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        hash_md5.update(f.read())
+    return hash_md5.hexdigest()
+
+def main():
+    files = glob.glob('*.nc')
+    for ncf in files:
+        prefix = ncf.split('.')[0]
+        outf = '{}.md5'.format(prefix)
+        with open(outf, 'w') as f:
+            f.write(file_md5_checksum(ncf))
+
+if __name__ == '__main__':
+    main()
